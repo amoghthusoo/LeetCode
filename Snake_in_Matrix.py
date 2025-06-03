64c40cd04230d3206271976c87c9bd37c17e0c5d
amoghthusoo <amoghthusoo@gmail.com>
2025-06-03 13:58:40 +0530

    Q514.1358.03.06.2025

---
+++ Snake_in_Matrix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: list[str]) -> int:
+        
+        i = j = 0
+        for command in commands:
+            if(command == "RIGHT"):
+                j += 1
+            elif(command == "LEFT"):
+                j -= 1
+            elif(command == "DOWN"):
+                i += 1
+            else:
+                i -= 1
+
+        return i * n + j
