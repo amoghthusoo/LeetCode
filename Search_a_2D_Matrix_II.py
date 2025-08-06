fe5004dba0d90d6af1e96a52a7e4afcfac9badba
Amogh Thusoo <amoghthusoo@gmail.com>
2025-08-06 22:15:47 +0530

    Q578.2215.06.08.2025

---
+++ Search_a_2D_Matrix_II.py
@@ -0,0 +1,19 @@
+from bisect import bisect_left
+
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        
+        for row in matrix:
+            index = bisect_left(row, target)
+
+            if(index < len(row) and row[index] == target):
+                return True
+            
+
+        return False
+
+matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+target = 5
+obj = Solution()
+result = obj.searchMatrix(matrix, target)
+print(result)
