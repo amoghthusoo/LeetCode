4a89ddd7bdcf9465fb3057bfc0091678dea01519
Amogh Thusoo <amoghthusoo@gmail.com>
2025-08-13 11:26:02 +0530

    updated solutions.py

---
+++ Revise/Power_of_Three.py
@@ -0,0 +1,22 @@
+from math import log, isclose
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+
+        if(n <= 0):
+            return False
+        
+        l = log(n, 3)
+        if(isclose(l, round(l), rel_tol = 0.000000000001)):
+            l = round(l)
+
+        if(int(l) == l):
+            return True
+        else:
+            return False
+
+
+n = 129140162
+obj = Solution()
+result = obj.isPowerOfTwo(n)
+print(result)        
