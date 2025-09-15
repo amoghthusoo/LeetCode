6291e0fff4b55182ef3e486977fc248d0a91eadd
Amogh Thusoo <amoghthusoo@gmail.com>
2025-09-15 16:16:32 +0530

    Q618.1616.15.09.2025

---
+++ Rotate_Function.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxRotateFunction(self, nums: list[int]) -> int:
+        
+        F0 = 0
+        _sum = 0
+        i = 0
+        while(i < len(nums)):
+
+            _sum += nums[i]
+            F0 += i * nums[i]
+            i += 1
+        
+        prev = _max = F0
+        i = 1
+        while(i < len(nums)):
+
+            Fi = prev + (_sum - nums[len(nums) - i]) - (len(nums) - 1) * nums[len(nums) - i]
+
+            if(Fi > _max):
+                _max = Fi
+            prev = Fi
+            i += 1
+
+        return _max
+
+nums = [4,3,2,6]
+obj = Solution()
+result = obj.maxRotateFunction(nums)
+print(result)
