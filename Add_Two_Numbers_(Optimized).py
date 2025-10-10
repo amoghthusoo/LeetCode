e11604faf7f765ad9fa875b26d4640893e492c84
Amogh Thusoo <amoghthusoo@gmail.com>
2025-10-10 10:13:25 +0530

    Q643.1013.10.10.2025

--- Add_Two_Numbers_(Optimized).py
+++ Add_Two_Numbers_(Optimized).py
@@ -1,110 +1,49 @@
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
-class LinkedList:
-
-    def insertAtEnd(self, head, value):
-        
-        temp = ListNode()
-        temp.val = value
-        temp.next = None
-
-        if(head == None):
-            head = temp
-        else:
-            traversePtr = head
-            
-            while(traversePtr.next != None):
-                traversePtr = traversePtr.next
-
-            traversePtr.next = temp
-        
-        return head
-    
-    def linkedListToList(self, head):
-
-        outList = []
-
-        traversePtr = head
-        while(traversePtr != None):
-            
-            outList.append(traversePtr.val)
-            traversePtr = traversePtr.next
-        
-        return outList
-    
-    def listToLinkedList(self, inList):
-        
-        outHead = None
-
-        for element in inList:
-            outHead = self.insertAtEnd(outHead, element)
-
-        return outHead
-    
-    def showLinkedList(self, head):
-
-        while(head != None):
-            print(f"{head.val} -> ", end="")
-            head = head.next
-
-        print(None)
-    
-    def linkedListToNum(self, head):
-        
-        traversePtr = head
-        num_str = ""
-        while(traversePtr != None):
-            num_str += str(traversePtr.val)
-            traversePtr = traversePtr.next
-    
-        return num_str
-        
-    def numToLinkedList(self, num):
-
-        outHead = None
-
-        for digit in num:
-            outHead = self.insertAtEnd(outHead, int(digit))
-
-        return outHead
-        
-
 class Solution:
 
-    def reverseNum(self, num):
+    def insert_at_end(self, head, tail, val):
 
-        outNum = ""
-        i = len(num) - 1
-        while(i >= 0):
+        temp = ListNode()
+        temp.val = val
 
-            outNum += num[i]           
-            i -= 1
-
-        return outNum
-
-    def addTwoNumbers(self, l1, l2):
+        if(head == tail == None):
+            head = tail = temp
         
-        linkedListOperations = LinkedList()
+        else:
+            tail.next = temp
+            tail = temp
         
-        l1_num = linkedListOperations.linkedListToNum(l1)
-        l2_num = linkedListOperations.linkedListToNum(l2)
-        l1_num = self.reverseNum(l1_num)
-        l2_num = self.reverseNum(l2_num)
-        l3_num = int(l1_num) + int(l2_num)
-        l3_num = self.reverseNum(str(l3_num))
-        l3 = linkedListOperations.numToLinkedList(l3_num)
+        return head, tail
 
-        return l3
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        num1 = ""
+        ptr = l1
+        while(ptr != None):
+            num1 += str(ptr.val)
+            ptr = ptr.next
+        
+        num2 = ""
+        ptr = l2
+        while(ptr != None):
+            num2 += str(ptr.val)
+            ptr = ptr.next
+        
 
-l1 = [5,6]
-l2 = [5,4,9]
-linkedListOperations = LinkedList()
-l1_head = linkedListOperations.listToLinkedList(l1)
-l2_head = linkedListOperations.listToLinkedList(l2)
+        num1 = num1[-1::-1]
+        num2 = num2[-1::-1]
+        res = str(int(num1) + int(num2))
+        res = res[-1::-1]
+        
 
-obj = Solution()
-outHead = obj.addTwoNumbers(l1_head, l2_head)
-outList = linkedListOperations.showLinkedList(outHead)
+        head = None
+        tail = None
+
+        for e in res:
+            head, tail = self.insert_at_end(head, tail, int(e))
+        
+        return head
---
+++ Taking_Maximum_Energy_From_the_Mystic_Dungeon.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumEnergy(self, energy: list[int], k: int) -> int:
+
+        ans = energy[-1]  
+
+        for i in range(len(energy) - 1, -1, -1):
+            
+            if(i + k < len(energy)):
+                energy[i] = energy[i] + energy[i + k]
+
+            if(energy[i] > ans):
+                ans = energy[i]
+        
+        return ans
+
+        
