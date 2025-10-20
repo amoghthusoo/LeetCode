405b11a9c0971305cf285ae0d64858b8fcc3a8b3
Amogh Thusoo <amoghthusoo@gmail.com>
2025-10-20 07:04:40 +0530

    updated solutions.py

--- Final_Value_of_Variable_After_Performing_Operations.py
+++ Final_Value_of_Variable_After_Performing_Operations.py
@@ -2,13 +2,11 @@ class Solution:
     def finalValueAfterOperations(self, operations: list) -> int:
         
         x = 0
-
         for element in operations:
             if element in ["++X", "X++"]:
                 x += 1
             else:
                 x -= 1
-
         return x
 
 operations = ["X++","++X","--X","X--"]
