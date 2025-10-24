942a145cbb045334665a07846550e8debe1ffc9c
Amogh Thusoo <amoghthusoo@gmail.com>
2025-10-24 11:15:20 +0530

    Q657.1115.24.10.2025

---
+++ Next_Greater_Numerially_Balanced_Number.py
@@ -0,0 +1,25 @@
+from collections import Counter
+class Solution:
+
+    def is_balanced(self, n):
+
+        freq_dict = Counter(str(n))
+
+        for d, f in freq_dict.items():
+            
+            if(f != int(d)):
+                return False
+        
+        return True
+    
+    def nextBeautifulNumber(self, n: int) -> int:
+
+        n += 1
+        while(not self.is_balanced(n)):
+            n += 1
+        return n    
+
+n = 3000
+obj = Solution()
+result = obj.nextBeautifulNumber(n)
+print(result)
