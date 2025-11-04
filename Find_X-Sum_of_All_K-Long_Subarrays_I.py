1244d6c9fc823a47850e1101f4cea1b58715b29a
Amogh Thusoo <amoghthusoo@gmail.com>
2025-11-04 10:52:13 +0530

    Q668.1052.04.11.2025

---
+++ Find_X-Sum_of_All_K-Long_Subarrays_I.py
@@ -0,0 +1,58 @@
+from collections import Counter
+
+class Solution:
+
+    def get_sum(self, arr, x):
+        
+        occr_dict = dict(Counter(arr))
+        occr_list = []
+
+        for e, f in occr_dict.items():
+            occr_list.append([e, f])
+
+        i = 0
+        while(i < len(occr_list) - 1):
+            
+            j = 0
+            while(j < len(occr_list) - 1 - i):
+               
+                if(occr_list[j][1] < occr_list[j + 1][1]):
+                    occr_list[j], occr_list[j + 1] = occr_list[j + 1], occr_list[j]
+                
+                elif(occr_list[j][1] == occr_list[j + 1][1]):
+                    if(occr_list[j][0] < occr_list[j + 1][0]):
+                        occr_list[j], occr_list[j + 1] = occr_list[j + 1], occr_list[j]
+                
+                j += 1            
+            i += 1
+
+        _sum = 0
+        i = 0
+        try:
+            while(i < x):
+                _sum += occr_list[i][0] * occr_list[i][1]
+                i += 1
+        except:
+            pass
+        
+        return _sum
+
+    def findXSum(self, nums: list[int], k: int, x: int) -> list[int]:
+        
+        answer = []
+
+        i = 0
+        while(i < len(nums) - k + 1):   
+            
+            answer.append(self.get_sum(nums[i : i + k], x))
+            i += 1
+
+        return answer
+
+nums = [9,2,2]
+k = 3
+x = 3
+
+obj = Solution()
+result = obj.findXSum(nums, k, x)
+print(result)
