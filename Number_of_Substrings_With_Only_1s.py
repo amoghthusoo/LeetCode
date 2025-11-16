3892efa4e0762bab2054e747d55559ca1e70ddef
Amogh Thusoo <amoghthusoo@gmail.com>
2025-11-16 09:13:34 +0530

    Q680.0913.16.11.2025

---
+++ Number_of_Substrings_With_Only_1s.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        
+        mod = int(10 ** 9 + 7)
+        ans = 0
+
+        count = 0
+        for i in range(len(s)):
+            if(s[i] == "1"):
+                count += 1
+            else:
+                ans += ((count * (count + 1)) // 2) % mod
+                count = 0
+        
+        ans += ((count * (count + 1)) // 2) % mod
+        
+        return ans
+
+s = "0110111" 
+obj = Solution()
+result = obj.numSub(s)
+print(result)
