a45380e5f97cc928eb1ffbf531450635802df6f9
Amogh Thusoo <amoghthusoo@gmail.com>
2025-12-11 09:25:58 +0530

    Q705.0925.11.12.2025

---
+++ Count_Covered_Buildings.py
@@ -0,0 +1,35 @@
+from sortedcontainers import SortedList
+class Solution:
+    def countCoveredBuildings(self, n: int, buildings: list[list[int]]) -> int:
+
+        dict_x = dict()
+        dict_y = dict()
+
+        for building in buildings:
+
+            if(building[0] not in dict_x):
+                dict_x[building[0]] = SortedList([building[1]])
+            else:
+                dict_x[building[0]].add(building[1])
+
+            if(building[1] not in dict_y):
+                dict_y[building[1]] = SortedList([building[0]])
+            else:
+                dict_y[building[1]].add(building[0])
+        
+        ans = 0        
+        for building in buildings:
+            x = building[0]
+            y = building[1]
+
+            if(dict_x[x][0] != y and dict_x[x][-1] != y and dict_y[y][0] != x and dict_y[y][-1] != x):
+                ans += 1
+        
+        return ans
+
+
+n = 5
+buildings = [[1,3],[3,2],[3,3],[3,5],[5,3]]
+obj = Solution()
+result = obj.countCoveredBuildings(n, buildings)
+print(result)
