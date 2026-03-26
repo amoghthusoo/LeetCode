67da7d6b755fcfe6c368467b6af3ac4dd045a8f8
Amogh Thusoo <amoghthusoo@gmail.com>
2026-03-26 23:27:40 +0530

    Q803.2327.26.03.2026

---
+++ Split_With_Minimum_Sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def splitNum(self, num: int) -> int:
+        
+        num = sorted(list(str(num)))
+        num1 = ""
+        num2 = ""
+
+        for i in range(len(num)):
+            
+            if(i % 2 == 0):
+                num1 += num[i]
+            else:
+                num2 += num[i]
+        
+        return int(num1) + int(num2)
