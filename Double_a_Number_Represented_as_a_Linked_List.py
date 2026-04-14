024d35d515b511ab05c79bd9daeceff1250423a5
Amogh Thusoo <amoghthusoo@gmail.com>
2026-04-14 16:17:14 +0530

    Q871.1617.14.04.2026

--- Double_a_Number_Represented_as_a_Linked_List.py
+++ Double_a_Number_Represented_as_a_Linked_List.py
@@ -1,82 +1,39 @@
+import sys
+sys.set_int_max_str_digits(100000)
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-class LinkedListOperations:
-
-    def insertAtEnd(self, head, element):
-        
-        temp = ListNode(element)
-
-        if(head == None):
-            head = temp
-        else:
-            traversePtr = head
-            while(traversePtr.next != None):
-                traversePtr = traversePtr.next
-            
-            traversePtr.next = temp
-        
-        return head
-
-    def displayLinkedList(self, head):
-        
-        traversePtr = head
-        while(traversePtr != None):
-            print(traversePtr.val, end=" -> ")
-            traversePtr = traversePtr.next
-        print(None)
-
-    def listToLinkedList(self, _list):
-        head = None
-
-        for element in _list:
-            head = self.insertAtEnd(head, element)
-
-        return head
-
 class Solution:
+    
+    def insertAtEnd(self, head, rear, val):
 
-    def insertAtEnd(self, head, element):
+        temp = ListNode()
+        temp.val = val
         
-        temp = ListNode(element)
+        if(head == rear == None):    
+            head = rear = temp
+            return head, rear
+        
+        rear.next = temp
+        rear = temp
 
-        if(head == None):
-            head = temp
-        else:
-            traversePtr = head
-            while(traversePtr.next != None):
-                traversePtr = traversePtr.next
-            
-            traversePtr.next = temp
+        return head, rear
+    
+    def doubleIt(self, head: ListNode) -> ListNode:
+
+        num = ""
+        ptr = head
+        while(ptr != None):
+            num += str(ptr.val)
+            ptr = ptr.next
+
+        num = str(int(num) * 2)
+
+        head = rear = None
+        for e in num:
+            head, rear = self.insertAtEnd(head, rear, int(e))
         
         return head
-    
-    def doubleIt(self, head):
-        
-        numStr = ""
-
-        traversePtr = head
-        while(traversePtr != None):
-            numStr += str(traversePtr.val)
-            traversePtr = traversePtr.next
-
-        numStr = str(int(numStr) * 2)
-
-        outHead = None
-
-        for ch in numStr:
-            outHead = self.insertAtEnd(outHead, int(ch))
-
-        return outHead
-
-
-_list = [9, 9, 9]
-oprObj = LinkedListOperations()
-head = oprObj.listToLinkedList(_list)
-
-obj = Solution()
-out = obj.doubleIt(head)
-oprObj.displayLinkedList(out)
