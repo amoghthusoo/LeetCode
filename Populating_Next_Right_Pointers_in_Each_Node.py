467a72db1060e3ba99f272357be053cd658e77cc
Amogh Thusoo <amoghthusoo@gmail.com>
2026-04-27 23:44:32 +0530

    Q932.2344.27.04.2026

---
+++ Populating_Next_Right_Pointers_in_Each_Node.py
@@ -0,0 +1,50 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+
+    def bfs(self, root):
+        
+        queue = [root]
+        bfs = []
+
+        while(len(queue) != 0):
+
+            temp = []
+            n = len(queue)
+            for _ in range(n):
+                
+                node = queue.pop(0)
+                temp.append(node)
+
+                if(node.left != None):
+                    queue.append(node.left)
+                
+                if(node.right != None):
+                    queue.append(node.right)
+            
+            bfs.append(temp)
+
+        return bfs
+    
+    def connect(self, root: Node) -> Node:
+
+        if(root == None):
+            return None
+        
+        levels = self.bfs(root)
+
+        for level in levels:
+            i = 0
+            while(i < len(level) - 1):
+                
+                level[i].next = level[i + 1]
+                i += 1
+
+        return levels[0][0]
