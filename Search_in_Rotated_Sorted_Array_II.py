37d0fd76afbd2963b222ee6b5c87640af3ba17e8
Amogh Thusoo <amoghthusoo@gmail.com>
2026-05-01 23:31:25 +0530

    Q946.2331.01.05.2026

--- Search_in_Rotated_Sorted_Array_II.py
+++ Search_in_Rotated_Sorted_Array_II.py
@@ -1,15 +1,7 @@
 class Solution:
-    def search(self, nums: list, target: int) -> bool:
+    def search(self, nums: list[int], target: int) -> bool:
         
-        if (target in nums):
+        if(target in nums):
             return True
         else:
             return False
-
-
-
-nums = [2,5,6,0,0,1,2]
-target = 3
-obj = Solution()
-solution = obj.search(nums, target)
-print(solution)
