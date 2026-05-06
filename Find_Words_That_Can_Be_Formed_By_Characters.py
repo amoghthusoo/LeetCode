247ce360014b0a083d3fda9eaffc19378ec79e85
Amogh Thusoo <amoghthusoo@gmail.com>
2026-05-06 21:24:38 +0530

    Q967.2124.06.05.2026

---
+++ Find_Words_That_Can_Be_Formed_By_Characters.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+class Solution:
+    def countCharacters(self, words: list[str], chars: str) -> int:
+        
+        chars_dict = dict(Counter(chars))
+        length = 0
+        
+        for word in words:
+
+            temp_dict = dict(Counter(word))
+            take_mem = True
+
+            for ch, occr in temp_dict.items():
+                
+                if(ch in chars_dict and occr <= chars_dict[ch]):
+                    pass
+                else:
+                    take_mem = False
+                    break
+
+            if(take_mem):
+                length += len(word)
+
+        return length
+    
+words = ["dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin","ndqeyhhcquplmznwslewjzuyfgklssvkqxmqjpwhrshycmvrb","ulrrbpspyudncdlbkxkrqpivfftrggemkpyjl","boygirdlggnh","xmqohbyqwagkjzpyawsydmdaattthmuvjbzwpyopyafphx","nulvimegcsiwvhwuiyednoxpugfeimnnyeoczuzxgxbqjvegcxeqnjbwnbvowastqhojepisusvsidhqmszbrnynkyop","hiefuovybkpgzygprmndrkyspoiyapdwkxebgsmodhzpx","juldqdzeskpffaoqcyyxiqqowsalqumddcufhouhrskozhlmobiwzxnhdkidr","lnnvsdcrvzfmrvurucrzlfyigcycffpiuoo","oxgaskztzroxuntiwlfyufddl","tfspedteabxatkaypitjfkhkkigdwdkctqbczcugripkgcyfezpuklfqfcsccboarbfbjfrkxp","qnagrpfzlyrouolqquytwnwnsqnmuzphne","eeilfdaookieawrrbvtnqfzcricvhpiv","sisvsjzyrbdsjcwwygdnxcjhzhsxhpceqz","yhouqhjevqxtecomahbwoptzlkyvjexhzcbccusbjjdgcfzlkoqwiwue","hwxxighzvceaplsycajkhynkhzkwkouszwaiuzqcleyflqrxgjsvlegvupzqijbornbfwpefhxekgpuvgiyeudhncv","cpwcjwgbcquirnsazumgjjcltitmeyfaudbnbqhflvecjsupjmgwfbjo","teyygdmmyadppuopvqdodaczob","qaeowuwqsqffvibrtxnjnzvzuuonrkwpysyxvkijemmpdmtnqxwekbpfzs","qqxpxpmemkldghbmbyxpkwgkaykaerhmwwjonrhcsubchs"]
+chars = "usdruypficfbpfbivlrhutcgvyjenlxzeovdyjtgvvfdjzcmikjraspdfp"
+obj = Solution()
+out = obj.countCharacters(words, chars)
+print(out)
