f59e738b3afc340b591df1fa7a0b9b82e4527915
Amogh Thusoo <amoghthusoo@gmail.com>
2026-05-11 19:39:13 +0530

    Q989.1939.11.05.2026

---
+++ Bitwise_OR_of_Even_Numbers_in_an_Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def evenNumberBitwiseORs(self, nums: list[int]) -> int:
+        
+        ans = 0
+
+        for num in nums:
+            if(num % 2 == 0):
+                ans |= num
+        
+        return ans
