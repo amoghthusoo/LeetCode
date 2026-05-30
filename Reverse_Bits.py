d7d80495badeabb41c6144a81f6df04851f65c0c
Amogh Thusoo <amoghthusoo@gmail.com>
2026-05-30 22:40:08 +0530

    Q1071.2240.30.05.2026

--- Reverse_Bits.py
+++ Reverse_Bits.py
@@ -1,24 +1,11 @@
-# NOT SOLVED
 class Solution:
     def reverseBits(self, n: int) -> int:
         
-        revStr : str = ""
-        decNum : int = 0
-        i = len(n) - 1
-        while (i >= 0):
-            revStr += n[i]
-            i -= 1
+        return int(bin(n)[2 : ].zfill(32)[-1 : : -1], 2)
+        
 
-        j = 0
-        while (j < len(revStr)):
 
-            decNum += int(revStr[j]) * 2 ** (len(revStr) - 1 - j)
-            j += 1
-        
-        return decNum
-        
-        
-n = "11111111111111111111111111111101"
+n = 43261596
 obj = Solution()
-solution = obj.reverseBits(n)
-print(solution)
+result = obj.reverseBits(n)
+print(result)
