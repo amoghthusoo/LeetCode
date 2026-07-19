9d8a548184e853bd18dd7d4f9c47837a6884edd9
Amogh Thusoo <amoghthusoo@gmail.com>
2026-07-19 14:17:03 +0530

    Q1144.1416.19.07.2026

---
+++ Smallest_Subsequence_of_Distinct_Characters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        
+        index_map = dict()
+        for i, ch in enumerate(s):
+            index_map[ch] = i
+        
+        seen = set()
+        stack = []
+
+        for i, ch in enumerate(s):
+
+            if(not stack):
+                stack.append(ch)
+                seen.add(ch)
+
+            elif(ch not in seen):
+
+                while(stack and stack[-1] > ch and index_map[stack[-1]] > i):
+                    seen.remove(stack.pop())
+                
+                stack.append(ch)
+                seen.add(ch)
+        
+        return "".join(stack)
