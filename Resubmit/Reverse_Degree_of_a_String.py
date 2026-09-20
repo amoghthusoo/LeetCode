099aa21a66dfd3dd12c85627ce8d38e8e5e50512
Amogh Thusoo <amoghthusoo@gmail.com>
2026-09-20 21:53:41 +0530

    Resubmit

---
+++ Resubmit/Reverse_Degree_of_a_String.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        reverse_degree = 0
+        for i, ch in enumerate(s):
+            reverse_degree += (123 - ord(ch)) * (i + 1)
+        return reverse_degree
